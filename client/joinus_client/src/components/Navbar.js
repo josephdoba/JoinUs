25ffeff0c5c590f5880ea5176165bedddf8ab00d
Kyler <kylerlepp8@gmail.com>
2022-09-21 14:02:05 -0700

    finished up navbar added dark light mode and added on page load animation

--- client/joinus_client/src/components/Navbar.js
+++ client/joinus_client/src/components/Navbar.js
@@ -1,13 +1,22 @@
 import React from "react";
 import './navbar.scss'
-const Navbar = function() {
+import logo from '../images/logo.png'
+import ReactSwitch from 'react-switch'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+const Navbar = function(props) {
   return (
     <div>
-      <nav className='nav'>
-        <img alt='LOGO' src='./images/logo'/>
-        <div></div>
+      <nav className='nav' id={props.theme}>
+        <main className='main-navbar'>
+        <img alt='LOGO' src={logo} className="logo"/>
+        <div className='login'><button className="but" id={props.theme}>Login</button>|</div>
+        <div className='signup'><button className="but" id={props.theme}>Signup</button></div>
+        <ReactSwitch className='toggle-switch' onChange={props.toggleTheme} checked={props.theme === 'dark'}/>
+        <FontAwesomeIcon icon={faUser} className='user-logo'/>
+        </main>
       </nav>
-    </div>  
+    </div>
   )
 }
 
