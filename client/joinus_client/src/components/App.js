e15c9c988bd9dd817a8c4ebe30e5629f814d4718
Kyler <kylerlepp8@gmail.com>
2022-09-20 17:41:01 -0700

    started working on navbar and installed some needed dependencies

--- client/joinus_client/src/components/App.js
+++ client/joinus_client/src/components/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import Navbar from "./Navbar";
 const App = function() {
   return (
     <div>
+      <Navbar/>
         <h1>App</h1>
     </div>
   )
---
+++ client/joinus_client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './navbar.scss'
+const Navbar = function() {
+  return (
+    <div>
+      <nav className='nav'>
+        <img alt='LOGO' src='./images/logo'/>
+        <div></div>
+      </nav>
+    </div>  
+  )
+}
+
+export default Navbar
