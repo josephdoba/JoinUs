cf0e13bb6242d232d399405f8299b7a372674616
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-21 22:16:17 -0700

    add eventlist component

---
+++ client/joinus_client/src/components/Eventlist.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Eventlist = () => {
+  const [eventData, setEventData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/events")
+      .then((data) => setEventData(data.events))
+      .catch((err) => console.error(err.response.data));
+  }, []);
+  return <div>{eventData}</div>;
+};
+
+export default Eventlist;
