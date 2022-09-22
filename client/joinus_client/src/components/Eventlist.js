08cc32a731d6d0603182f83af3a2d1a62cd5c4d0
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-21 22:20:33 -0700

    comment out buggy code

--- client/joinus_client/src/components/Eventlist.js
+++ client/joinus_client/src/components/Eventlist.js
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Eventlist = () => {
-  const [eventData, setEventData] = useState({});
+  // const [eventData, setEventData] = useState({});
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/api/events")
-      .then((data) => setEventData(data.events))
-      .catch((err) => console.error(err.response.data));
-  }, []);
-  return <div>{eventData}</div>;
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:8080/api/events")
+  //     .then((data) => setEventData(data.events))
+  //     .catch((err) => console.error(err.response.data));
+  // }, []);
+  return <div>Hello from eventlist!</div>;
 };
 
 export default Eventlist;
