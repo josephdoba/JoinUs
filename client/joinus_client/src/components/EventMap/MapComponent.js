194dd527483fed2ba847f90970f764fb8c7eb89a
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-24 08:58:07 -0700

    move rendering the actual map to its own component

--- client/joinus_client/src/components/EventMap/MapComponent.js
+++ client/joinus_client/src/components/EventMap/MapComponent.js
@@ -1,5 +1,16 @@
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, Marker } from "@react-google-maps/api";
+
+const center = {
+  lat: 51.0233064354121,
+  lng: -114.02369425973428,
+};
 
 export default function MapComponent() {
-  return <div>in Map Component</div>;
+  // const [map, setMap] = useState(null);
+
+  return (
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <Marker position={center} />
+    </GoogleMap>
+  );
 }
