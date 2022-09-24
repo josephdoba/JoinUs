bb541dd10a7a16ace3d8ef161cf262d562e770dd
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-24 08:49:09 -0700

    attempt load event map

--- client/joinus_client/src/components/EventMap/index.js
+++ client/joinus_client/src/components/EventMap/index.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import MapComponent from "./MapComponent";
 import MapError from "./MapError";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import "./map.scss";
+
+const center = {
+  lat: 51.0233064354121,
+  lng: -114.02369425973428,
+};
 
 const EventMap = () => {
-  return <div>in Map Component</div>;
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
+
+  const [map, setMap] = useState(null);
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <MapComponent />;
+
+  function MapComponent() {
+    return (
+      <GoogleMap
+        zoom={10}
+        center={center}
+        mapContainerClassName="map-container"
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    );
+  }
 };
 
 export default EventMap;
