94e57ef0ccfdcc4672f7703cee74509cf7eaaa7f
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2022-09-25 03:32:56 -0400

    Create Feed component to load event

---
+++ client/joinus_client/src/components/Feed.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function Feed() {
+  return (
+    <Box bgcolor={"red"}
+      flex={4}
+      p={2}
+    >
+      feed
+    </Box>
+  );
+}
