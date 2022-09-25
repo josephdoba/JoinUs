3994f60190fc8738e8903583b190421839d97ae4
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-25 09:35:15 -0700

    remove dummy data & style events

--- client/joinus_client/src/components/Event.js
+++ client/joinus_client/src/components/Event.js
@@ -8,6 +8,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 import moment from "moment";
+import { Grid } from "@mui/material";
 
 export default function Event(props) {
   const { name, image, description, start_time, end_time, category } = props;
@@ -16,27 +17,29 @@ export default function Event(props) {
   const end = moment(end_time).format("LT");
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia component="img" alt={name} height="140" image={image} />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {name}
-        </Typography>
-        <Typography gutterBottom variant="body2" color="text.secondary">
-          {start} - {end} <br />
-          Category: {category}
-        </Typography>
+    <Grid item xs={4}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia component="img" alt={name} height="140" image={image} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography gutterBottom variant="body2" color="text.secondary">
+            {start} - {end} <br />
+            Category: {category}
+          </Typography>
 
-        <Typography paragraph>{description}</Typography>
-      </CardContent>
-      <CardActions>
-        <Button onClick={() => {}} size="small">
-          Learn More
-        </Button>
-        <Button onClick={() => {}} size="small">
-          Join Event
-        </Button>
-      </CardActions>
-    </Card>
+          <Typography paragraph>{description}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button onClick={() => {}} size="small">
+            Learn More
+          </Button>
+          <Button onClick={() => {}} size="small">
+            Join Event
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 }
--- client/joinus_client/src/components/Eventlist.js
+++ client/joinus_client/src/components/Eventlist.js
@@ -1,41 +1,8 @@
-import { Container } from "@mui/material";
+import { Container, Grid } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
 import React, { useState, useEffect } from "react";
 import Event from "./Event";
-import Stack from "@mui/material/Stack";
 import { fetchAPI } from "../api";
-
-const events = [
-  {
-    id: 1,
-    name: "coffee chat",
-    image:
-      "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p.jpg",
-    description:
-      "Come join me for me a lovely quick little morning coffee and chat about the problems of the world",
-    size_limit: 4,
-    category: "food",
-    owner_id: 1,
-    latitude: 51.0233064354121,
-    longitude: -114.02369425973428,
-    start_time: "2022-09-28 05:00:00",
-    end_time: "2022-09-28 16:00:00",
-  },
-  {
-    id: 2,
-    name: "board games",
-    image:
-      "https://i.cbc.ca/1.2716999.1406221490!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/135000779-board-games.jpg",
-    description:
-      "Come join me for some fun board games this could include battleship, monopoly, and risk!",
-    size_limit: 4,
-    category: "games",
-    owner_id: 1,
-    latitude: 49.25825320517397,
-    longitude: -123.04434376344798,
-    start_time: "2022-09-28 08:00:00",
-    end_time: "2022-09-28 11:00:00",
-  },
-];
 
 export default function Eventlist() {
   const [eventData, setEventData] = useState([]);
@@ -49,7 +16,7 @@ export default function Eventlist() {
       .catch((err) => console.error(err.response.data));
   }, []);
 
-  const event = eventData.map((e) => {
+  const events = eventData.map((e) => {
     return (
       <Event
         key={e.id}
@@ -65,13 +32,13 @@ export default function Eventlist() {
 
   return (
     <Container>
-      <h2 id="homepage-eventlist-title">Find an event!</h2>
-      <Stack
-        direction={{ xs: "column", sm: "row" }}
-        spacing={{ xs: 1, sm: 2, md: 4 }}
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
       >
-        {event}
-      </Stack>
+        {events}
+      </Grid>
     </Container>
   );
 }
