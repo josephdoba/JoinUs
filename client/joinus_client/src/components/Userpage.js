559316e962a95444046df70487116f08072e8866
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2022-09-25 06:01:08 -0400

    Update

--- client/joinus_client/src/components/Userpage.js
+++ client/joinus_client/src/components/Userpage.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Sidebar from './Sidebar'
-import Feed from './Feed'
+import Feedlist from './Feedlist'
 import App_navbar from "./App_navbar";
 import { Box, Stack } from "@mui/material";
 
-export default function Homepage() {
+export default function Userpage() {
   return (
     <Box>
       <App_navbar />
       <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
         <Sidebar />
-        <Feed />
+        <Feedlist />
       </Stack>
     </Box>
   );
