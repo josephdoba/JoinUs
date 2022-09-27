ef7e8eeae4f116cd167aa4c9ac383d752a65aca5
Kyler <kylerlepp8@gmail.com>
2022-09-27 16:40:03 -0700

    created the chat app and created the route just need the chat button implemented then i can add the route for it

--- client/joinus_client/src/components/App.js
+++ client/joinus_client/src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import FunPhoto from "./Home/FunPhotos";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Chat from "./Chat";
 
 import "./app.scss";
 
@@ -21,21 +23,24 @@ const App = function () {
   };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <div id={theme}>
-        <Navbar
-          toggleTheme={toggleTheme}
-          theme={theme}
-          success={success}
-          setSuccess={setSuccess}
-          user={user}
-          setUser={setUser}
-        />
-        <FunPhoto/>
-        <Home />
-        <IndividualEvent />
-      </div>
-    </ThemeContext.Provider>
+    <Router>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <div id={theme}>
+          <Navbar
+            toggleTheme={toggleTheme}
+            theme={theme}
+            success={success}
+            setSuccess={setSuccess}
+            user={user}
+            setUser={setUser}
+          />
+          <Routes>
+            <Route path='/' element={<div><FunPhoto /> <Home /> <IndividualEvent /> </div> } />
+            <Route path='/event/chat' element={<Chat user={user}/>} />
+          </Routes>
+        </div>
+      </ThemeContext.Provider>
+    </Router>
   );
 };
 
