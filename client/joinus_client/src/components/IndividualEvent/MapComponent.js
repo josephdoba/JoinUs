6f17f7b718aa94c50a827601db47f565942a396f
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2022-09-29 01:00:41 -0400

    add style

--- client/joinus_client/src/components/IndividualEvent/MapComponent.js
+++ client/joinus_client/src/components/IndividualEvent/MapComponent.js
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import { GoogleMap, Marker } from "@react-google-maps/api";
 
 const center = {
@@ -9,8 +10,11 @@ export default function MapComponent() {
   // const [map, setMap] = useState(null);
 
   return (
+    <Box bgcolor="red" flex={"50%"} p={2}>
+
     <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
       <Marker position={center} />
-    </GoogleMap>
+    </GoogleMap >
+    </Box>
   );
 }
