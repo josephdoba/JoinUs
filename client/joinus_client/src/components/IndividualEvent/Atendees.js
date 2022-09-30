699cb2c4d4ae7a4dca88a68bd17c5bc66d2055fc
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-30 13:01:24 -0700

    create avatars for attendee list

---
+++ client/joinus_client/src/components/IndividualEvent/Atendees.js
@@ -0,0 +1,18 @@
+import { Avatar, AvatarGroup } from "@mui/material";
+
+export default function Attendees(props) {
+  const { attendeelist } = props;
+
+  const avatar = attendeelist.map((person) => {
+    return <Avatar alt={person.name} src={person.picture} />;
+  });
+
+  return (
+    <AvatarGroup
+      max={5}
+      sx={{ "& .MuiAvatar-root": { width: 24, height: 24, fontSize: 15 } }}
+    >
+      {avatar}
+    </AvatarGroup>
+  );
+}
