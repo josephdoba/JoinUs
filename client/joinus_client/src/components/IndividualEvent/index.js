980c1e24d6d7600abac3f1681111e80d280a5242
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-29 20:09:19 -0700

    load map location for event

--- client/joinus_client/src/components/IndividualEvent/index.js
+++ client/joinus_client/src/components/IndividualEvent/index.js
@@ -4,12 +4,11 @@ import EventMap from "./EventMap";
 
 export default function IndividualEvent(props) {
   const { event } = props;
-  console.log(event);
+
   return (
     <Box flex={"row"}>
       <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
-        <EventDetails />
-        <p>{event.name}</p>
+        <EventDetails event={event} />
         <EventMap lat={event.lat} lng={event.lng} />
       </Stack>
     </Box>
