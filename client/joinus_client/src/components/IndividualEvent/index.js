71cd24968663217dd37f39c02abeb37f89fda402
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2022-09-30 02:08:46 -0400

    update

--- client/joinus_client/src/components/IndividualEvent/index.js
+++ client/joinus_client/src/components/IndividualEvent/index.js
@@ -1,17 +1,30 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Fab, Stack, Typography } from "@mui/material";
+import { bgcolor } from "@mui/system";
 import EventDetails from "./EventDetails";
 import EventMap from "./EventMap";
+import EventMapDetails from "./EventMapDetails";
+import NavigationIcon from '@mui/icons-material/Navigation';
+import JoinEventButton from "./JoinEventButton";
 
 export default function IndividualEvent(props) {
   const { event } = props;
 
   return (
-    <Box flex={"row"}>
-      <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
-        <EventDetails />
-        <p>{event.name}</p>
-        <EventMap />
-      </Stack>
+    <Box flex={"row"} >
+      <Box display="flex" justifyContent="center" alignItems="center" m={3}>
+        <Typography variant="h4">
+          Here EVENT NAME{/* {event.name} */}
+        </Typography>
+      </Box>
+      <Box>
+        <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
+          <EventDetails />
+          <EventMapDetails />
+        </Stack>
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'row-reverse', m: 5}}>
+        <JoinEventButton />
+      </Box>
     </Box>
   );
 }
