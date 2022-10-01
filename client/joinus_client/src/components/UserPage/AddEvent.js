36927c818edaae2b7d1f210316d25ed9da5c59c9
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2022-10-01 18:40:07 -0400

    upload img

--- client/joinus_client/src/components/UserPage/AddEvent.js
+++ client/joinus_client/src/components/UserPage/AddEvent.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import dayjs from 'dayjs';
 import userEvents from '../../api/userEvents';
+import CategoriesList from "./CategoriesList";
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
 
 import {
   Button,
   Fab,
   IconButton,
+  Input,
   Modal,
   Stack,
   styled,
@@ -20,7 +23,7 @@ import { Box } from "@mui/system";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 
 export default function AddEvent() {
-   const { userCreateEventSubmit } = userEvents()
+  const { userCreateEventSubmit } = userEvents()
 
   const StyledModal = styled(Modal)({
     display: "flex",
@@ -70,22 +73,22 @@ export default function AddEvent() {
       >
         {/* https://stackoverflow.com/questions/29791721/how-get-data-from-material-ui-textfield-dropdownmenu-components */}
         {/* https://stackoverflow.com/questions/65531477/how-to-post-form-data-using-material-ui-into-api */}
-        
-          <Box width={500} height={500} bgcolor="white" p={3} borderRadius={3}>
-            <Typography variant="h6" color="gray" textAlign="center">
-              Create New Event
-            </Typography>
-            <FormBox
-              component="form"
-              // 
-              sx={{
-                "& > :not(style)": { m: 1, width: "100%" },
-              }}
-              noValidate
-              autoComplete="off"
-              >
-              {/* https://stackoverflow.com/questions/59862828/how-to-connect-button-to-form-submission-using-material-ui-cards */}
-              {/*
+
+        <Box width={500} height={700} bgcolor="white" p={3} borderRadius={3}>
+          <Typography variant="h6" color="gray" textAlign="center">
+            Create New Event
+          </Typography>
+          <FormBox
+            component="form"
+            // 
+            sx={{
+              "& > :not(style)": { m: 1, width: "100%" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            {/* https://stackoverflow.com/questions/59862828/how-to-connect-button-to-form-submission-using-material-ui-cards */}
+            {/*
 
               omg Moe help how tf does this form/submit interaction work in MUI?!
               tried putting "onSubmit={userCreateEventSubmit}" as a property within the <FormBox> component.. wasn't working.  
@@ -93,49 +96,67 @@ export default function AddEvent() {
             Hmmm.. mkay so using "<form onSubmit={userCreateEventSubmit}>" "technically" works at even hitting the "userCreateEventSubmit function", but it breaks the entire layout. 
 
               */}
-            
-              <TextField
-                id="standard-basic"
-                label="Event Name"
-                variant="standard"
+
+            <TextField
+              id="standard-basic"
+              label="Event Name"
+              variant="standard"
+            />
+
+            <TextField
+              id="standard-basic"
+              label="Full Address"
+              variant="standard"
+            />
+
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimePicker
+                label="Time"
+                value={value}
+                onChange={(handleChange)}
+                renderInput={(params) => <TextField {...params} />}
               />
+            </LocalizationProvider>
 
-              <TextField
-                id="standard-basic"
-                label="Full Address"
-                variant="standard"
+            <CategoriesList/>
+
+            <TextField
+              id="outlined-textarea"
+              label="Event Details"
+              placeholder="..."
+              multiline
+              inputProps={{ maxLength: 300 }}
+            />
+
+            <Stack direction="row" justifyContent="left" >
+
+              <input
+                accept="image/*"
+                style={{ display: 'none' }}
+                id="raised-button-file"
+                type="file"
               />
-
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <TimePicker
-                  label="Time"
-                  value={value}
-                  onChange={(handleChange)}
-                  renderInput={(params) => <TextField {...params} />}
-                />
-              </LocalizationProvider>
-
-              <TextField
-                id="outlined-textarea"
-                label="Event Details"
-                placeholder="..."
-                multiline
-                inputProps={{ maxLength: 300 }}
-              />
-
-              <Stack direction="row" spacing={2} justifyContent="center" >
-                <Button onClick={(e) => setOpen(false)} variant="outlined">
-                  Cancel
+              <label htmlFor="raised-button-file">
+                <Button variant="text" component="span" endIcon={<AddIcon />}>
+                  Upload Image
                 </Button>
-                {/* <Button variant="contained" endIcon={<AddIcon />}> */}
-                {/* <Button variant="contained" type="submit" endIcon={<AddIcon />}> */}
-                <Button variant="contained" type="submit" endIcon={<AddIcon />} onClick={userCreateEventSubmit}>
-                  Create
-                </Button>
-              </Stack>
+              </label>
 
-            </FormBox>
-          </Box>
+            </Stack>
+
+            <Stack direction="row" spacing={2} justifyContent="center" >
+              <Button onClick={(e) => setOpen(false)} variant="outlined">
+                Cancel
+              </Button>
+              {/* <Button variant="contained" endIcon={<AddIcon />}> */}
+              {/* <Button variant="contained" type="submit" endIcon={<AddIcon />}> */}
+              <Button variant="contained" type="submit" endIcon={<AddIcon />} onClick={userCreateEventSubmit}>
+                Create
+              </Button>
+            </Stack>
+
+          </FormBox>
+        </Box>
       </StyledModal>
     </>
   );
