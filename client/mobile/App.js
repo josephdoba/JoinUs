7d87875f1c4908f13117ab9ffcebefe3b10e03cb
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-02 11:25:24 -0700

    load home and User page routes

--- client/mobile/App.js
+++ client/mobile/App.js
@@ -6,85 +6,44 @@
  * @flow strict-local
  */
 
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
 import {
+  View,
+  Text,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import HomeScreen from './src/components/Home/Home';
 import Navbar from './src/components/Navbar';
 import UserScreen from './src/components/User/UserScreen';
+import HomeScreen from './src/components/Home';
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
+const Stack = createNativeStackNavigator();
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const Stack = createNativeStackNavigator();
-
   return (
     <NavigationContainer>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
+      {/* <SafeAreaView> */}
+      {/* <StatusBar /> */}
+      {/* <ScrollView> */}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Home'}}
         />
-        <Navbar />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <View>
-            <Stack.Navigator>
-              <Stack.Screen
-                name="Home"
-                component={HomeScreen}
-                options={{title: 'Welcome'}}
-              />
-              <Stack.Screen name="User" component={UserScreen} />
-            </Stack.Navigator>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+        <Stack.Screen name="User" component={UserScreen} />
+      </Stack.Navigator>
+      {/* </ScrollView> */}
+      {/* </SafeAreaView> */}
     </NavigationContainer>
   );
 };
--- client/mobile/android/app/src/main/java/com/mobile/MainActivity.java
+++ client/mobile/android/app/src/main/java/com/mobile/MainActivity.java
@@ -4,6 +4,9 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
+//added for react navigation
+import android.os.Bundle;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -43,6 +46,11 @@ public class MainActivity extends ReactActivity {
       // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
       // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
       return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+    }
+    @Override
+      // added for react navigation
+      protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(null);
     }
   }
 }
