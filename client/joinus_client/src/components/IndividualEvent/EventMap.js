3c9ec85664a7004455cea9868abbd24ff5b38a2e
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-02 20:51:17 -0700

    auto-complete to get lat/lng!!!!

--- client/joinus_client/src/components/IndividualEvent/EventMap.js
+++ client/joinus_client/src/components/IndividualEvent/EventMap.js
@@ -3,13 +3,20 @@ import MapComponent from "./MapComponent";
 import MapError from "./MapError";
 import { useLoadScript } from "@react-google-maps/api";
 import EventMapDetails from "./EventMapDetails";
-import { Box, Stack, Button, Input, ButtonGroup } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
 } from "use-places-autocomplete";
-import useOnclickOutside from "react-cool-onclickoutside";
 
 const libraries = ["places"];
 
@@ -20,6 +27,7 @@ export default function EventMap(props) {
   });
   const { start_time, end_time, lat, lng } = props;
   const [map, setMap] = useState(/** @type googlemaps.Map */ (null));
+  const [selected, setSelected] = useState(null);
 
   if (loadError) return <MapError />;
   if (!isLoaded) return <div>Loading...</div>;
@@ -32,8 +40,8 @@ export default function EventMap(props) {
   return (
     <Box flex={"30%"} p={2}>
       <Stack direction={"column"} spacing={2} justifyContent={"space-between"}>
-        <Search />
-        <MapComponent lat={lat} lng={lng} setMap={setMap} />
+        <Search setSelected={setSelected} />
+        <MapComponent lat={lat} lng={lng} setMap={setMap} selected={selected} />
 
         <EventMapDetails
           start_time={start_time}
@@ -43,14 +51,47 @@ export default function EventMap(props) {
           handleClick={handleClick}
         />
       </Stack>
+      <p>{selected && selected.lat}</p>
+      <p>{selected && selected.lng}</p>
     </Box>
   );
 }
 
-function Search() {
-  const {} = usePlacesAutocomplete({
-    requsetOptions: {
-      location: { lat },
-    },
-  });
+function Search({ setSelected }) {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    const results = await getGeocode({ address });
+    const { lat, lng } = await getLatLng(results[0]);
+    setSelected({ lat, lng });
+  };
+
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        className="combobox-input"
+        placeholder="Address"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
 }
