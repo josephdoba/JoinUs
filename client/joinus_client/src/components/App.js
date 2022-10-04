83f1bc40b92aca84d0ae818f6163232454549291
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-03 18:32:17 -0700

    separate out search

--- client/joinus_client/src/components/App.js
+++ client/joinus_client/src/components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createContext, useState } from "react";
 import Home from "./Home";
-import FunPhoto from "./Home/FunPhotos";
 import Chat from "./Chat";
 import Userpage from "./UserPage/index";
 import "./app.scss";
@@ -18,6 +17,7 @@ const App = function () {
   const [theme, setTheme] = useState("light");
   const [user, setUser] = useState({});
   const [event, setEvent] = useState({});
+  const [selected, setSelected] = useState(null);
 
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
@@ -65,6 +65,8 @@ const App = function () {
                   categoriesData={categoriesData}
                   setEvent={setEvent}
                   setUser={setUser}
+                  setSelected={setSelected}
+                  selected={selected}
                 />
               }
             />
--- client/joinus_client/src/components/IndividualEvent/EventMap.js
+++ client/joinus_client/src/components/IndividualEvent/EventMap.js
@@ -4,19 +4,7 @@ import MapError from "./MapError";
 import { useLoadScript } from "@react-google-maps/api";
 import EventMapDetails from "./EventMapDetails";
 import { Box, Stack, ListItemText } from "@mui/material";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
-import "@reach/combobox/styles.css";
-
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
+import Search from "./Search";
 
 const libraries = ["places"];
 
@@ -58,41 +46,3 @@ export default function EventMap(props) {
 }
 
 // Autocomplete box for search.
-function Search({ setSelected }) {
-  const {
-    ready,
-    value,
-    setValue,
-    suggestions: { status, data },
-    clearSuggestions,
-  } = usePlacesAutocomplete();
-
-  const handleSelect = async (address) => {
-    setValue(address, false);
-    clearSuggestions();
-
-    const results = await getGeocode({ address });
-    const { lat, lng } = await getLatLng(results[0]);
-    setSelected({ lat, lng });
-  };
-
-  return (
-    <Combobox onSelect={handleSelect}>
-      <ComboboxInput
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        disabled={!ready}
-        className="combobox-input"
-        placeholder="Address"
-      />
-      <ComboboxPopover>
-        <ComboboxList>
-          {status === "OK" &&
-            data.map(({ place_id, description }) => (
-              <ComboboxOption key={place_id} value={description} />
-            ))}
-        </ComboboxList>
-      </ComboboxPopover>
-    </Combobox>
-  );
-}
