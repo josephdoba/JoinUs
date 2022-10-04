2e60a71645a4666b09376fe0a163c920feea58fc
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-03 21:03:40 -0700

    set id for list

--- client/joinus_client/src/components/UserPage/CategoriesList.js
+++ client/joinus_client/src/components/UserPage/CategoriesList.js
@@ -1,24 +1,26 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 export default function CategoriesList(props) {
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState("");
 
   const handleChange = (event) => {
     setCategory(event.target.value);
   };
 
-  const { categoriesData } = props
+  const { categoriesData } = props;
 
   const categories = categoriesData.map((category) => {
     return (
-      <MenuItem value={category.id}>{category.name}</MenuItem>
-    )
-  })
+      <MenuItem value={category.id} id={category.id}>
+        {category.name}
+      </MenuItem>
+    );
+  });
   return (
     <Box sx={{ minWidth: 120 }}>
       <FormControl fullWidth>
