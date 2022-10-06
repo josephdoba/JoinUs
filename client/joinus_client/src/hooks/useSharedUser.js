cda1533bc5beb2a1697a8b3ff6cb57242dc5c124
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-05 22:27:28 -0700

    add new user data to state

--- client/joinus_client/src/hooks/useSharedUser.js
+++ client/joinus_client/src/hooks/useSharedUser.js
@@ -2,7 +2,13 @@ import { useBetween } from "use-between";
 import { useState } from "react";
 
 const useUser = () => {
-  const [user, setUser] = useState({ id: null, name: null, picture: null });
+  const [user, setUser] = useState({
+    id: null,
+    name: null,
+    age: null,
+    gender: null,
+    picture: null,
+  });
   console.log(`user in shareduser ${user.id}`);
   return {
     user,
