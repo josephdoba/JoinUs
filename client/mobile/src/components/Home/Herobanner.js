6c0dc8a855165f51bda42a51702f10ef0bcd09da
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-06 21:29:53 -0700

    align items center

--- client/mobile/src/components/Home/Herobanner.js
+++ client/mobile/src/components/Home/Herobanner.js
@@ -1,9 +1,9 @@
-import {Text, StyleSheet, Image} from 'react-native';
+import {Text, StyleSheet, Image, View} from 'react-native';
 import React from 'react';
 
 const Herobanner = () => {
   return (
-    <>
+    <View style={styles.container}>
       <Text style={styles.title}>Social without the schedule.</Text>
       <Image
         style={styles.bannerImage}
@@ -12,20 +12,24 @@ const Herobanner = () => {
       <Text style={styles.subtitle}>
         Make new friends with shared interests using local short-term meetups.
       </Text>
-    </>
+    </View>
   );
 };
 
 export default Herobanner;
 
 const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    alignItems: 'center',
+  },
   title: {
     fontWeight: 'bold',
     fontSize: 30,
     color: 'dimgray',
   },
   subtitle: {
-    fontSize: 25,
+    fontSize: 20,
     color: 'dimgray',
   },
   bannerImage: {
