04d5d687a269b839fc424a3da9be31858ce5d018
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-06 18:59:01 -0700

    update blurb

--- client/mobile/src/components/Home/Herobanner.js
+++ client/mobile/src/components/Home/Herobanner.js
@@ -4,21 +4,13 @@ import React from 'react';
 const Herobanner = () => {
   return (
     <>
-      <Image
-        style={styles.logo}
-        source={require('../../images/logo_light.png')}
-      />
+      <Text style={styles.title}>Social without the schedule.</Text>
       <Image
         style={styles.bannerImage}
         source={require('../../images/running.png')}
       />
-      <Text style={styles.body}>
-        Have you ever been out in public and wanted to experience something new
-        with someone new? Tired of making plans weeks in advance only to have
-        them cancel at the last minute? Do you miss the spontaneity of your
-        childhood?! Well look no further! Our app JoinUs! is a short-term meetup
-        app that allows users to arrange short-term, spontaneous meetups with
-        other like minded individuals. No commitment required!
+      <Text style={styles.subtitle}>
+        Make new friends with shared interests using local short-term meetups.
       </Text>
     </>
   );
@@ -32,7 +24,8 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: 'dimgray',
   },
-  bodytext: {
+  subtitle: {
+    fontSize: 25,
     color: 'dimgray',
   },
   bannerImage: {
