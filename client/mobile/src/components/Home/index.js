903eeffb57785472c6701d8e61ca159d7718b804
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-07 12:21:06 -0700

    update to button group

--- client/mobile/src/components/Home/index.js
+++ client/mobile/src/components/Home/index.js
@@ -1,40 +1,50 @@
-import {Button} from '@rneui/base';
+import {Button, ButtonGroup} from '@rneui/base';
 import React, {useState} from 'react';
 import {ScrollView, StyleSheet} from 'react-native';
 
 import Herobanner from './Herobanner';
 import HowTo from './HowTo';
 
+import useAppData from '../../hooks/useAppData';
+
 const HomeScreen = ({navigation}) => {
+  const {usersData} = useAppData();
+
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
   const [user, setUser] = useState({
-    id: 1,
-    name: 'Lawrence Orr',
-    age: 24,
-    gender: 'male',
-    picture: 'https://cdn-icons-png.flaticon.com/128/4333/4333609.png',
+    id: null,
+    name: null,
+    age: null,
+    gender: null,
+    picture: null,
   });
+  console.log(user);
+
+  const handleSelect = async value => {
+    setSelectedIndex(value);
+    if (value === 1) {
+      await navigation.navigate('User', {name: 'Carmen'});
+    }
+  };
 
   return (
     <ScrollView styles={styles.sectionContainer}>
       <Herobanner />
       <HowTo />
-      <Button
-        title="Log in"
-        loading={false}
-        loadingProps={{size: 'small', color: 'white'}}
-        buttonStyle={elementStyle.login}
-        titleStyle={elementStyle.title}
-        containerStyle={elementStyle.container}
-        onPress={() => navigation.navigate('User', {name: 'Carmen'})}
-      />
-      <Button
-        title="Sign up!"
-        loading={false}
-        loadingProps={{size: 'small', color: 'white'}}
+
+      <ButtonGroup
+        buttons={['Sign Up', 'Log In']}
+        selectedIndex={selectedIndex}
         buttonStyle={elementStyle.signup}
-        titleStyle={elementStyle.title}
+        selectedButtonStyle={elementStyle.login}
+        onPress={value => {
+          handleSelect(value);
+        }}
+        onPressOut={value => {
+          setSelectedIndex(null);
+        }}
         containerStyle={elementStyle.container}
-        onPress={() => console.log('aye')}
       />
     </ScrollView>
   );
@@ -43,18 +53,19 @@ const HomeScreen = ({navigation}) => {
 const elementStyle = {
   login: {
     backgroundColor: 'rgba(111, 202, 186, 1)',
-    borderRadius: 5,
+    // borderRadius: 5,
   },
   signup: {
     backgroundColor: '#F9CF93',
-    borderRadius: 5,
+    // borderRadius: 5,
   },
   title: {fontWeight: 'bold', fontSize: 23},
   container: {
     marginHorizontal: 50,
     height: 50,
-    width: 200,
     marginVertical: 10,
+    marginBottom: 50,
+    marginTop: 20,
   },
 };
 
