ce1afd1b01cc3568e3162b5576694ad4c23abef9
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-06 21:30:11 -0700

    create flatlist item

--- client/mobile/src/components/Home/HowTo.js
+++ client/mobile/src/components/Home/HowTo.js
@@ -8,66 +8,80 @@ import {
   StatusBar,
   Image,
 } from 'react-native';
+import {Card} from '@rneui/base';
+import Herobanner from './Herobanner';
 
-const INSTRUCTIONS = [
+const DATA = [
   {
     id: 1,
-    image: (
-      <Image
-        style={styles.imageSize}
-        source={require('../../images/join.png')}
-      />
-    ),
-    alt: 'how to join',
-    title: 'Join Us!',
+    image: require('../../images/sign_up.png'),
+    title: 'Join',
     description: 'Create an account and see what others around are doing!',
   },
   {
     id: 2,
-    image: (
-      <Image
-        style={styles.imageSize}
-        source={require('../../images/idea.png')}
-      />
-    ),
-    alt: 'create',
+    image: require('../../images/idea.png'),
     title: 'Create',
     description:
       'Think of something fun to do and make an event or join someone in theirs',
   },
   {
     id: 3,
-    image: (
-      <Image
-        style={styles.imageSize}
-        source={require('../../images/chat.png')}
-      />
-    ),
-    alt: 'Chat',
+    image: require('../../images/chat.png'),
     title: 'Chat',
     description:
       'Chat with others to make sure you get along and confirm your plans.',
   },
   {
     id: 4,
-    image: (
-      <Image
-        style={styles.imageSize}
-        source={require('../../images/meet.png')}
-      />
-    ),
-    alt: 'Meet',
+    image: require('../../images/meet.png'),
     title: 'Meet',
     description: 'Head out and do something new with a new friend.',
   },
 ];
-const HowTo = ({}) => {};
+
+const Instructions = ({image, title, description}) => (
+  <Card containerStyle={{}} wrapperStyle={{}}>
+    <Image style={styles.imageSize} resizeMode="contain" source={image} />
+    <Card.Title>{title}</Card.Title>
+    <Text>{description}</Text>
+  </Card>
+);
+const HowTo = () => {
+  const renderInstruction = ({item}) => (
+    <Instructions
+      title={item.title}
+      image={item.image}
+      description={item.description}
+    />
+  );
+
+  return (
+    <>
+      <FlatList
+        data={DATA}
+        renderItem={renderInstruction}
+        keyExtractor={item => item.id}
+      />
+    </>
+  );
+};
 
 export default HowTo;
 
-const styles = Stylesheet.create({
+const styles = StyleSheet.create({
   imageSize: {
-    height: 60,
-    width: 60,
+    height: 100,
+    // width: 60,
+    width: '100%',
+    marginBottom: 8,
+  },
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  cardView: {
+    position: 'relative',
+    alignItems: 'center',
   },
 });
