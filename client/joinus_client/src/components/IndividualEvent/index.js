e399f4f46a2aed21228114e6e0d9a747fa0d29c3
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2022-10-07 20:24:54 -0400

    move join event button to EventDetails component

--- client/joinus_client/src/components/IndividualEvent/index.js
+++ client/joinus_client/src/components/IndividualEvent/index.js
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import { findEventAttendees } from "../../helpers/user_selectors";
 import useSharedEvent from "../../hooks/useSharedEvent";
 
@@ -15,7 +15,8 @@ export default function IndividualEvent(props) {
   const attendeelist = findEventAttendees(event.id, usersData, joinedEvents);
 
   return (
-    <Box flex={"row"}>
+
+    <Box flex={"row"}  m={2}>
       <Box display="flex" justifyContent="center" alignItems="center" m={3}>
         <Typography variant="h4">{event.name}</Typography>
       </Box>
@@ -24,9 +25,6 @@ export default function IndividualEvent(props) {
           <EventDetails attendeelist={attendeelist} />
           <EventMap />
         </Stack>
-      </Box>
-      <Box sx={{ display: "flex", flexDirection: "row-reverse", m: 5 }}>
-        <JoinEventButton />
       </Box>
     </Box>
   );
