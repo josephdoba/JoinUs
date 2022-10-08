c21a8e2bfe5d093ac69b81aaf7bcb407d803f18f
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2022-10-07 20:13:52 -0400

    Change the component to Button component

--- client/joinus_client/src/components/IndividualEvent/JoinEventButton.js
+++ client/joinus_client/src/components/IndividualEvent/JoinEventButton.js
@@ -1,20 +1,10 @@
-import { Fab, Tooltip } from "@mui/material";
-
+import { Button, Fab, Tooltip } from "@mui/material";
 import LocalActivityIcon from "@mui/icons-material/LocalActivity";
 
 export default function JoinEventButton() {
   return (
-    <Tooltip
-      onClick={(e) => {}}
-      title="Create A New Event"
-      sx={{
-        position: "inherit",
-      }}
-    >
-      <Fab variant="extended">
-        <LocalActivityIcon sx={{ mr: 1 }} />
-        Join Event
-      </Fab>
-    </Tooltip>
+    <Button size="medium" color="primary"  endIcon={<LocalActivityIcon />}>
+      Join Event
+    </Button>
   );
 }
