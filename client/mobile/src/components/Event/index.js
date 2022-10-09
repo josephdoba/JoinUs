82960e1c538a27bf5775c0ce1bc218c0927e98db
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-09 02:16:25 -0700

    render name for event on screen

--- client/mobile/src/components/Event/index.js
+++ client/mobile/src/components/Event/index.js
@@ -3,9 +3,10 @@ import React from 'react';
 import {Button} from '@rneui/themed';
 
 const EventScreen = ({navigation, route}) => {
+  const event = route.params;
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Event Screen</Text>
+      <Text>{event.name}</Text>
       <Button title="Go Back" onPress={() => navigation.goBack()} />
     </View>
   );
